56badc9e6bf40e0bed2d4c242cd7d07432f17ce8
Davidvoklol <polyak.daavid@gmail.com>
2023-04-10 15:52:40 +0200

    Summary

---
+++ Game.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+
+class Game:
+    def __init__(self):
+        
+
+        self.window = tk.Tk()
+        self.window.geometry("500x250")
+        self.window.title("Window")
+        self.window.mainloop()
